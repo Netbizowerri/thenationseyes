9e4d223a8cdb00fc762012fa049c5bc5e0d69f83
Netbizowerri <netbiz0925@gmail.com>
2026-03-29 11:26:07 +0100

    Migrate from HashRouter to BrowserRouter for clean URL routing

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, Link, useLocation, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import ArticleDetail from './pages/ArticleDetail';
 import AdminDashboard from './pages/AdminDashboard';
